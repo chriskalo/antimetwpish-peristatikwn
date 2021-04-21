16972b7a22ba6ff655662281bdc8d8e0c20c56cf
Christodoulos <43905544+chriskalo@users.noreply.github.com>
2021-04-21 11:21:39 +0300

    Update scripts.js

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -42,7 +42,7 @@ function startTimer(duration, display) {
             }
 
             isPlaying = false;
-            display.innerHTML = "<h1>Πόντοι: " + points + "</h1><p>Αυτό ήταν!!! όταν είναι έτοιμος ο επόμενος παίκτης μπορεί να πατήσει το κουμπί.</p>";
+            display.innerHTML = "<h1>Πόντοι: " + points + "/12</h1><p>Αυτό ήταν!!! όταν είναι έτοιμος ο επόμενος παίκτης μπορεί να πατήσει το κουμπί.</p>";
             document.getElementById("questions").style.display = "none";
             points = 0;
         }
@@ -53,11 +53,51 @@ function startTimer(duration, display) {
 function results(){
   var q1 = document.myform.q1.value;
   var q2 = document.myform.q2.value;
+  var q3 = document.myform.q3.value;
+  var q4 = document.myform.q4.value;
+  var q5 = document.myform.q5.value;
+  var q6 = document.myform.q6.value;
+  var q7 = document.myform.q7.value;
+  var q8 = document.myform.q8.value;
+  var q9 = document.myform.q9.value;
+  var q10 = document.myform.q10.value;
+  var q11 = document.myform.q11.value;
+  var q12 = document.myform.q12.value;
 
   if (q1 == "a"){
     points++;
   }
   if (q2 == "a"){
+    points++;
+  }
+  if (q3 == "a"){
+    points++;
+  }
+  if (q4 == "c"){
+    points++;
+  }
+  if (q5 == "b"){
+    points++;
+  }
+  if (q6 == "c"){
+    points++;
+  }
+  if (q7 == "c"){
+    points++;
+  }
+  if (q8 == "a"){
+    points++;
+  }
+  if (q9 == "c"){
+    points++;
+  }
+  if (q10 == "b"){
+    points++;
+  }
+  if (q11 == "c"){
+    points++;
+  }
+  if (q12 == "b"){
     points++;
   }
 }
@@ -72,7 +112,7 @@ document.getElementById("finishButton").addEventListener("click", function() {
   }
 
   isPlaying = false;
-  document.querySelector('#timer').innerHTML = "<h1>Πόντοι: " + points + "</h1><p>Αυτό ήταν!!! όταν είναι έτοιμος ο επόμενος παίκτης μπορεί να πατήσει το κουμπί.</p>";
+  document.querySelector('#timer').innerHTML = "<h1>Πόντοι: " + points + "/12</h1><p>Αυτό ήταν!!! όταν είναι έτοιμος ο επόμενος παίκτης μπορεί να πατήσει το κουμπί.</p>";
   document.getElementById("questions").style.display = "none";
   points = 0;
 
